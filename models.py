34b1ad1fc1de780c66463db2e8e418b3051708a0
guy <guyklinger94@gmail.com>
2021-11-11 15:26:27 +0200

    data preparation infrastructure

--- models.py
+++ models.py
@@ -6,3 +6,16 @@ class DTC:
     def __init__(self):
 
         self.model = DecisionTreeClassifier(random_state=0)
+
+
+class MissingDataTrainDTC:
+
+    def __init__(self):
+
+        self.model = DecisionTreeClassifier(random_state=0)  # override .fit function
+
+
+class MissingDataPredictionDTC:
+
+    def __init__(self):
+        self.model = DecisionTreeClassifier(random_state=0)  # override .predict function
