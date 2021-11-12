79e81b53abe0aa61cf3a9b048abee9f86167bece
guy <guyklinger94@gmail.com>
2021-11-12 14:53:24 +0200

    data preparation infrastructure

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import read_csv as reader
 from data_preparation import DataPreparation
 from models import DTC, MissingDataTrainDTC, MissingDataPredictionDTC
+from sklearn.metrics import roc_auc_score
 
 
 def iterate_files(impute=True):
@@ -10,11 +11,14 @@ def iterate_files(impute=True):
     """
 
     file_names = reader.read_file_names('data')
+    total_data = {}
     for name in file_names:
         dp = DataPreparation()
         dp.df = reader.read_file(name)
         prepare_data(dp, impute)
-        return dp
+        total_data[name] = dp
+
+    return total_data
 
 
 def prepare_data(dp, impute):
@@ -31,21 +35,25 @@ def prepare_data(dp, impute):
 
 
 def train_model(class_model, x_train, y_train):
-    class_model.model.fit(x_train, y_train)
+    class_model.fit(x_train, y_train)
 
 
 def run_complete_model():
-    prepared_data = iterate_files(impute=True)
 
+    total_data = iterate_files(impute=True)
     dtc = DTC()  # 'normal' DecisionTreeClassifier
-    train_model(dtc, prepared_data.x_train, prepared_data.y_train)
-    y_prediction = dtc.model.predict(prepared_data.x_test)
-    return y_prediction, prepared_data.y_test  # return both for evaluation
+    scores = []
+
+    for name, prepared_data in total_data.items():
+        train_model(dtc, prepared_data.x_train, prepared_data.y_train)
+        y_prediction = dtc.predict(prepared_data.x_test)
+        scores.append([name, evaluate(y_prediction, prepared_data.y_test)])  # return both for evaluation
+    return scores
 
 
-def run_missing_values_in_prediction_model():  # gonen working on this
+def run_missing_values_in_prediction_model():
 
-    # TODO:
+    # TODO: gonen working on this
     # make sure that after data is prepared (prepared_data instance)
     # we need to omit values in y_test, x_test.
     # then, override predict() method -> and predict
@@ -53,10 +61,9 @@ def run_missing_values_in_prediction_model():  # gonen working on this
     raise NotImplemented
     class_model = MissingDataPredictionDTC()
     prepared_data = iterate_files(impute=True)
-    train_model(class_model.model, prepared_data.x_train, prepared_data.y_train)
-    y_prediction = class_model.model.predict(prepared_data.x_test)
+    train_model(class_model, prepared_data.x_train, prepared_data.y_train)
+    y_prediction = class_model.predict(prepared_data.x_test)
     return y_prediction, prepared_data.y_test  # return both for evaluation
-
 
 
 def run_missing_values_in_training_model():  # guy working on this
@@ -67,16 +74,30 @@ def run_missing_values_in_training_model():  # guy working on this
     # then, override fit() method -> and train
 
     class_model = MissingDataTrainDTC()
-    prepared_data = iterate_files(impute=False)
-    prepared_data.y_train = prepared_data.add_missing_data_10_percent(prepared_data.y_train.to_frame())
-    prepared_data.x_train = prepared_data.add_missing_data_10_percent(prepared_data.x_train)
+    total_data = iterate_files(impute=False)
+    scores = []
 
-    train_model(class_model.model, prepared_data.x_train, prepared_data.y_train)
-    y_prediction = class_model.model.predict(prepared_data.x_test)
-    return y_prediction, prepared_data.y_test  # return both for evaluation
+    for name, prepared_data in total_data.items():
+        prepared_data.x_train = prepared_data.add_missing_data_10_percent(prepared_data.x_train)
+        train_model(class_model, prepared_data.x_train, prepared_data.y_train)
+        y_prediction = class_model.predict(prepared_data.x_test)
+        scores.append([name, evaluate(y_prediction, prepared_data.y_test)])
+
+    return scores
+
+
+def evaluate(y_test, y_pred):
+    try:
+        return roc_auc_score(y_test, y_pred)
+    except ValueError:
+        pass
 
 
 if __name__ == '__main__':
-    y_prediction_complete_model, y_test_complete_model = run_complete_model()
+    complete_model_scores = run_complete_model()
     # y_prediction_missing_predicion_values_model, y_test_missing_prediction_values_model = run_missing_values_in_prediction_model()
-    y_prediction_missing_train_values_model, y_test_missing_train_values_model = run_missing_values_in_training_model()
+    missing_train_values_model_scores = run_missing_values_in_training_model()
+
+    print('')
+    print(f'Complete model AUROC: {complete_model_scores}\n')
+    print(f'Missing values during train model AUROC: {missing_train_values_model_scores}')
